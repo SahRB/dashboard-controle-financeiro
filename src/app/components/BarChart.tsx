dfcf635fd6d51f1ece841e5352321c208508f0d9
Sabrina Barros <sabrina7410@hotmail.com.br>
2024-07-02 23:57:17 -0300

    Implementação de Grafico de barras da relacao de valores de entrada e saida por data

---
+++ src/app/components/BarChart.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import { BarChart } from '@mui/x-charts/BarChart'; // Verifique se este é o import correto
+import Stack from '@mui/material/Stack';
+
+interface Transaction {
+  date: string;
+  type: 'Entrada' | 'Saída';
+  value: number;
+}
+
+interface Props {
+  transactions: Transaction[];
+}
+
+interface BarSeriesType {
+  data: (number | null)[];
+  stack: string;
+  label: string;
+}
+
+const BarChartComponent: React.FC<Props> = ({ transactions }) => {
+  const aggregatedData: { [key: string]: { Entrada: number; Saída: number } } = {};
+
+  transactions.forEach(({ date, type, value }) => {
+    const dateString = new Date(date).toLocaleDateString();
+    if (!aggregatedData[dateString]) {
+      aggregatedData[dateString] = { Entrada: 0, Saída: 0 };
+    }
+    aggregatedData[dateString][type] += value;
+  });
+
+  // Transforma aggregatedData em um array de objetos para o dataset
+  const dataset = Object.entries(aggregatedData).map(([date, values]) => ({
+    date,
+    Entrada: values.Entrada,
+    Saída: values.Saída,
+  }));
+
+  const series = [
+    { dataKey: 'Entrada', label: 'Entrada' },
+    { dataKey: 'Saída', label: 'Saída' },
+  ];
+
+  return (
+    <Stack direction="column" spacing={1} sx={{ width: '100%', maxWidth: 600 }}>
+      <BarChart
+       
+      colors={['#188348', '#E47880']}
+      dataset={dataset}
+      xAxis={[{ scaleType: 'band', dataKey: 'date', tickLabelStyle: { fill: 'white' }, }]}
+      yAxis={[{ scaleType: 'linear', tickLabelStyle: { fill: 'white' }, }]} 
+      sx={{ fill: 'white' }}
+      borderRadius={5}
+      series={series}
+      width={400}
+      height={300}
+      slotProps={{
+        legend: {
+          labelStyle: {
+            fontSize: 13,
+            fill: 'white',
+           
+          },
+        },
+      }}
+        
+      />
+    </Stack>
+  );
+};
+
+export default BarChartComponent;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,19 +2,21 @@
 import React, { useEffect, useState } from 'react';
 import Card from "../components/Card";
 import Pie from '../components/Pie';
+import BarChartComponent from '../components/BarChart';
 
 interface Transaction {
   id: number;
-  type: string;
+  type: 'Entrada' | 'Saída';
   note: string;
   category: string;
   value: number;
+  date: string; 
 }
 
 const Dashboard = () => {
-  const [despesas, setDespesas] = useState(0);
-  const [renda, setRenda] = useState(0);
-  const [saldo, setSaldo] = useState(0);
+  const [despesas, setDespesas] = useState<number>(0);
+  const [renda, setRenda] = useState<number>(0);
+  const [saldo, setSaldo] = useState<number>(0);
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
   useEffect(() => {
@@ -25,7 +27,7 @@ const Dashboard = () => {
     const totalRenda = realTransactions.filter(t => t.type === 'Entrada').reduce((acc, curr) => acc + curr.value, 0);
     const totalSaldo = totalRenda - totalDespesas;
 
-    setTransactions(realTransactions); 
+    setTransactions(realTransactions);
     setDespesas(totalDespesas);
     setRenda(totalRenda);
     setSaldo(totalSaldo);
@@ -39,14 +41,18 @@ const Dashboard = () => {
         <Card title="Renda" value={`R$ ${renda}`} />
         <Card title="Saldo" value={`R$ ${saldo}`} />
       </div>
-      <div className='grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 justify-items-center pl-20 '>
-        <div className='mt-8'>
-        <h5 className='text-center'>Saidas por categoria</h5>
-        <Pie transactions={transactions.filter(t => t.type === 'Saída')} />
+      <div className='grid grid-cols-1 justify-items-center mt-8'>
+        <h2 >Relação de entrada e saida por data</h2>
+        <BarChartComponent transactions={transactions} />
+      </div>
+      <div className='grid grid-cols-1 pl-10 sm:grid-cols-1 lg:grid-cols-2 justify-items-center mt-4'>
+        <div >
+          <h5 className='text-center'>Saídas por categoria</h5>
+          <Pie transactions={transactions.filter(t => t.type === 'Saída')} />
         </div>
-        <div className='mt-8 '>
-        <h5 className='text-center'>Entradas por categoria</h5>
-      <Pie transactions={transactions.filter(t => t.type === 'Entrada')} />
+        <div >
+          <h5 className='text-center'>Entradas por categoria</h5>
+          <Pie transactions={transactions.filter(t => t.type === 'Entrada')} />
         </div>
       </div>
     </main>
